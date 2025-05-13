5b2d5cf45c7e78001c05214b531f6b8d170d66cf
Christopher G Parent <chris.parent@outlook.com>
2025-05-12 22:26:14 -0400

    Some progress made.  Adding Events.

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,18 @@
-export default function page() {
-  return <div>Events Page </div>;
+import H1 from "@/components/h1";
+
+type EventPageProps = {
+  params: { city: string };
+};
+
+export default function EventsPage({ params }: EventPageProps) {
+  const { city } = params;
+  return (
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>
+        {city === "all" && "All Events"}
+        {city !== "all" &&
+          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+    </main>
+  );
 }
---
+++ src/components/search-form.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function SearchForm() {
+  const [searchText, setSearchText] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const input = event.currentTarget.querySelector("input");
+    if (input) {
+      const query = input.value;
+      if (query) {
+        router.push(`/events/${query}`);
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="w-full sm:w-[580px]">
+      <input
+        className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        placeholder="Search events in any city..."
+        spellCheck="false"
+      />
+    </form>
+  );
+}
