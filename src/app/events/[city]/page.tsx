5f04d62d4a12c89c8d66cb372899395494361953
Christopher Parent <chris.parent@outlook.com>
2025-05-13 23:29:49 -0400

    Worked on the event cards

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -7,16 +7,15 @@ type EventPageProps = {
 };
 
 export default async function EventsPage({ params }: EventPageProps) {
+  const { city } = params;
   const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
   const events: EventoEvent[] = await response.json();
 
-  const { city } = params;
-
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
-      <H1>
+      <H1 className="mb-28">
         {city === "all" && "All Events"}
         {city !== "all" &&
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
