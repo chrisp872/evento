3a516acefa720104451c0c562f9c41acbf6e1073
Christopher G. Parent <chris.parent@outlook.com>
2025-05-13 22:28:05 -0400

    Sync Commit.  Create new components for events.

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,11 +1,19 @@
+import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
 
 type EventPageProps = {
   params: { city: string };
 };
 
-export default function EventsPage({ params }: EventPageProps) {
+export default async function EventsPage({ params }: EventPageProps) {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+  );
+  const events: EventoEvent[] = await response.json();
+
   const { city } = params;
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1>
@@ -13,6 +21,8 @@ export default function EventsPage({ params }: EventPageProps) {
         {city !== "all" &&
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
+
+      <EventsList events={events} />
     </main>
   );
 }
